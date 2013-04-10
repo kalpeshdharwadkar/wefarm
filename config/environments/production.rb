dad3a9db71af658a33388e7421f393551c499ab5
vasusen <vasusen@gmail.com>
2013-04-10 16:34:27 -0700

    adding copying development settings to production settings for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,11 @@ Wefarm::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  CLIENT_ID = 32636
+  CLIENT_SECRET = "180c800c62"
+  USE_STAGE = true
+  WEPAY = WePay.new(CLIENT_ID, CLIENT_SECRET, USE_STAGE)
+  
+  $host = $host || 'http://staging.wefarm.com'
 end
